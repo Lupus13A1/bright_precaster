7926675751380fafa85677f9a3f77aeb2ebb03a8
Thanet <symphoniga@gmail.com>
2024-05-28 21:23:41 +0700

    first commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,66 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "หน้าแรก",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "สินค้า",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "ผลลงาน",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "เกี่ยวกับ",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Pages",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "About Page",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Contact Page",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Blog Grid Page",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Blog Sidebar Page",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Blog Details Page",
+        path: "/blog-details",
+        newTab: false,
+      },
+    ],
+  },
+];
+export default menuData;
