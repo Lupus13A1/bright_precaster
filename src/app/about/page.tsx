be7bb491a3a1286e2cfffd66d6ae1c02a3b5da7a
Lupus13A1 <symphoniga@gmail.com>
2024-05-30 13:56:00 +0700

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,7 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
+  title: "About"
   // other metadata
 };
 
@@ -14,8 +13,8 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="About"
+        description="Bright Precaster Co.,Ltd ปัจจุบัน บริษัทพัฒนาอสังหาริมทรัพย์ หลายราย ได้ทำการก่อสร้าง โครงการต่างๆโดยใช้ระบบ Wall Bearing System เพื่อความรวดเร็วในการก่อสร้าง ได้ผลงานที่มีคุณภาพ ลดจำนวนแรงงานก่อสร้างลง เนื่องจากแรงงานที่มีคุณภาพหายากขึ้น และด้วยข้อดีของการใช้ผลิตภัณฑ์คอนกรีตเสริมเหล็กสำเร็จรูปข้างต้น ทำให้เป็นที่ยอมรับจากผู้ประกอบการในการเปลี่ยนการก่อสร้างจากระบบเดิมมาใช้ระบบนี้กันมากขึ้นเรื่อยๆ จึงส่งผลกระทบทำให้การสรรหาผู้ผลิตชิ้นส่วนคอนกรีตสำเร็จรูปที่มีคุณภาพ ทำได้ยากลำบากขึ้น"
       />
       <AboutSectionOne />
       <AboutSectionTwo />
--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -28,20 +28,18 @@ const AboutSectionTwo = () => {
             <div className="max-w-[470px]">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Bug free code
+                กำลังการผลิต
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                เครื่องจักร 1 ไลน์การผลิต มีกำลังการผลิต 1,000 ตรม.ต่อวัน (ชั่วโมงทำงานที่ 8 ชั่วโมงต่อวัน/2กะ) ดังนั้นจะสามารถผลิตแผ่นคอนกรีตสำเร็จรูปได้ประมาณ 300,000 ตร.ม.ต่อปี กำลังการผลิตดังกล่าวสามารถสร้างทาวน์เฮ้าส์ขนาด 2 ชั้น พื้นที่ใช้สอย 110 ตร.ม. ได้ประมาณ 1,050 ยูนิต หรือคอนโดมิเนียมขนาดห้อง 28 ตรม. ได้ประมาณ 2,700 ยูนิต
                 </p>
               </div>
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Premier support
+                สถานที่ตั้ง
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
+                บริษัท ไบร์ท พรีแคสเตอร์ ตั้งอยู่เลขที่ 10/10 หมู่ 10 ต.บางบัวทอง อ.บางบัวทอง จ.นนทบุรี 11110 พื้นที่ประกอบด้วยอาคารสำนักงานและอาคารโรงงาน ขนาด 5,544 ตรม.และ Stock yard ขนาด 4,500 ตรม. บนพื้นที่ 44-1-23 ไร่ เครื่องจักรที่ใช้จะเป็นระบบ Semi Automated Carrousel System ซึ่งเป็นระบบการผลิตชิ้นส่วนคอนกรีตเสริมเหล็กสำเร็จรูปอัตโนมัติ นำเข้าเครื่องจักรจากประเทศเยอรมัน
                 </p>
               </div>
               <div className="mb-1">
