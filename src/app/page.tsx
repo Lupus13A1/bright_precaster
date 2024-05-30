d2be94873ebb6ea7474216da9cf3da50fef22d22
Lupus13A1 <symphoniga@gmail.com>
2024-05-30 09:44:55 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,7 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Home",
   // other metadata
 };
 
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,24 +1,25 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Breadcrumb from "@/components/Common/Breadcrumb";
-import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Contact "
+  title: "Product"
   // other metadata
 };
 
-const ContactPage = () => {
+const ProductPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="Contact Page"
+        pageName="Product Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
-
-      <Contact />
+      <AboutSectionOne />
+      <AboutSectionTwo />
     </>
   );
 };
 
-export default ContactPage;
+export default ProductPage;
