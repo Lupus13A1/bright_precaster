163e9c8334fd4991ca372832114affa60a5a1d47
Thanet <symphoniga@gmail.com>
2024-06-22 15:23:47 +0700

    commit

--- src/components/translation/translations.js
+++ src/components/translation/translations.js
@@ -5,8 +5,34 @@ export const translations = {
     "Manufacturer and distributor of precast concrete components (Precast Concrete System) complete with a Semi Automated Carrousel System, an automated precast production system imported from Germany.":
       "Manufacturer and distributor of precast concrete components (Precast Concrete System) complete with a Semi Automated Carrousel System, an automated precast production system imported from Germany.",
     carrousel:
-      "The CARROUSEL system is an automated precast panel production system with high efficiency. It is divided into stations, each of which performs a specific function in the production process. The system is designed to optimize workflow and minimize production time, resulting in significant cost savings.",
+      "Carrousel system is a highly efficient, fully automatic precast production system. divided into stations Each station performs one specific function in the production process as follows.",
     plotter: "Plotter",
+    spreader: "Spreader",
+    compacting: "Compacting",
+    smoothing: "Smoothing",
+    curing: "Curing",
+    tilting: "Tilting",
+    plotter_dt:
+      "Responsible for drawing the shape of the workpiece and the specified equipment, which is a crucial step in defining the workpiece scope and production sequence.",
+    spreader_dt:
+      "Responsible for loading and transporting concrete precisely to the designated points.",
+    compacting_dt:
+      "Responsible for vibrating and shaking the pallet to allow concrete to reach every part of the workpiece, reducing voids in the workpiece. The intensity of shaking depends on the workpiece.",
+    smoothing_dt:
+      "After the workpiece has been compacted, it is smoothed using a trowel to ensure that the concrete surface is smooth and uniform, adhering evenly to the formwork.",
+    curing_dt:
+      "Controls temperature and humidity to achieve the designed strength of the concrete.",
+    tilting_dt:
+      "After curing, the hydraulic system tilts the pallet and workpiece to facilitate lifting.",
+    production: "Production Characteristics",
+    production_dt:
+      "Precasting in Reinforced Concrete (Precasting in Reinforced Concrete) is the process of casting concrete with steel reinforcement to achieve the desired shape, corresponding to the mold shape created. The reinforcement of steel is done simultaneously with the concrete casting. Once cast, the workpiece cannot be modified. It is then transported to construction sites for assembly.",
+    production_capacity: "Production Capacity",
+    production_capacity_dt:
+      "Each production line has a capacity of producing 1,000 square meters per day (8 hours per day/2 shifts), totaling approximately 300,000 square meters per year. This production capacity can construct approximately 2-storey townhouses with a floor area of 110 square meters, totaling around 1,050 units, or condominiums with 28-square-meter rooms, totaling approximately 2,700 units.",
+    location: "Location",
+    location_dt:
+      "Bright Precaster Co., Ltd. is located at 10/10 Moo 10, Bang Bua Thong Subdistrict, Bang Bua Thong District, Nonthaburi 11110. The premises include office buildings and factory buildings totaling 5,544 square meters, and a stock yard of 4,500 square meters, on an area of 44-1-23 rai. The machinery used is a Semi Automated Carrousel System imported from Germany, specializing in the production of precast reinforced concrete components.",
   },
   th: {
     "Bright Precaster Co.,Ltd.": "บริษัท ไบรท์พรีแคสเตอร์ จำกัด",
@@ -16,5 +42,30 @@ export const translations = {
     carrousel:
       "ระบบ CARROUSEL เป็นระบบการผลิตแผ่นพรีคาสท์แบบอัตโนมัติที่มีประสิทธิภาพสูง แบ่งออกเป็นสถานี แต่ละสถานีจะทำหน้าที่เฉพาะอย่างหนึ่งในกระบวนการผลิต ดังนี้",
     plotter: "เครื่องวาดแบบ (Plotter)",
+    spreader: "เทคอนกรีต (Spreader)",
+    compacting: "การทำให้คอนกรีตแน่น (Compacting)",
+    smoothing: "ขัดผิวหน้า (Smoothing)",
+    curing: "ห้องบ่มคอนกรีต(Curing)",
+    tilting: "ถอดออกจากแบบ (Tilting)",
+    plotter_dt:
+      "ทำหน้าที่วาดรูปร่างของชิ้นงาน และอุปกรณ์ที่กำหนดไว้ ซึ่งขั้นตอนนี้เป็นขั้นตอนที่จะสำคัญในการกำหนดขอเขตชิ้นงาน และลำดับการผลิต",
+    spreader_dt: "ทำหน้าที่ บรรจุและขนคอนกรีตมาตรงที่จุดพร้อมเท",
+    compacting_dt:
+      "ทำหน้าที่จะสั่นและเขย่า Pallet เพื่อให้คอนกรีตสามารถเข้าถึงทุกส่วนของชิ้นงานได้ ช่วยให้ลดการเกิดช่องว่างในชิ้นงาน ความแรงในการเขย่าขึ้นกับชิ้นงาน",
+    smoothing_dt:
+      "หลังจากที่ชิ้นงานผ่านการ Compacting แล้วจะนำชิ้นงานมาปาดหน้าผิวด้วยใบพัด เพื่อให้คอนกรีตเรียบสวยและเสมอกับไม้แบบให้ทั่วทั้งชิ้นงาน",
+    curing_dt:
+      "ทำหน้าที่ควบคุมอุณภูมิและความชื้น เพื่อให้คอนกรีตได้ค่าความแข็งแรงตามที่ออกแบบไว้",
+    tilting_dt:
+      "หลังจากบ่มเสร็จแล้วไฮโดริกยกพาเรทและชิ้นงานเอียง เพื่อให้ยกชิ้นงานได้ง่าย",
+    production: "ลักษณะการผลิต",
+    production_dt:
+      "เป็นการหล่อชิ้นส่วนคอนกรีตเสริมเหล็กสำเร็จรูป (Precasting in Reinforced Concrete) คือ การหล่อคอนกรีตที่มีการเสริมเหล็กเพื่อให้ได้รูปทรงตามที่ต้องการ ตรงกับรูปทรงของแม่แบบ (Mould Shape) ที่สร้างไว้ ซึ่งการเสริมเหล็กจะทำพร้อมกับการหล่อแบบคอนกรีต เมื่อหล่อแบบออกมาเป็นชิ้นส่วนงานแล้ว จะไม่สามารถดัดแปลงรูปแบบได้อีก แล้วจึงทำการขนย้ายไปประกอบยังหน่วยงานก่อสร้าง",
+    production_capacity: "กำลังการผลิต",
+    production_capacity_dt:
+      "เครื่องจักร 1 ไลน์การผลิต มีกำลังการผลิต 1,000 ตรม.ต่อวัน (ชั่วโมงทำงานที่ 8 ชั่วโมงต่อวัน/2กะ) ดังนั้นจะสามารถผลิตแผ่นคอนกรีตสำเร็จรูปได้ประมาณ 300,000 ตร.ม.ต่อปี กำลังการผลิตดังกล่าวสามารถสร้างทาวน์เฮ้าส์ขนาด 2 ชั้น พื้นที่ใช้สอย 110 ตร.ม. ได้ประมาณ 1,050 ยูนิต หรือคอนโดมิเนียมขนาดห้อง 28 ตรม. ได้ประมาณ 2,700 ยูนิต",
+    location: "สถานที่ตั้ง",
+    location_dt:
+      "บริษัท ไบร์ท พรีแคสเตอร์ จำกัด ตั้งอยู่เลขที่ 10/10 หมู่ 10 ต.บางบัวทอง อ.บางบัวทอง จ.นนทบุรี 11110 พื้นที่ประกอบด้วยอาคารสำนักงานและอาคารโรงงาน ขนาด 5,544 ตรม.และ Stock yard ขนาด 4,500 ตรม. บนพื้นที่ 44-1-23 ไร่ เครื่องจักรที่ใช้จะเป็นระบบ Semi Automated Carrousel System ซึ่งเป็นระบบการผลิตชิ้นส่วนคอนกรีตเสริมเหล็กสำเร็จรูปอัตโนมัติ นำเข้าเครื่องจักรจากประเทศเยอรมัน",
   },
 };
