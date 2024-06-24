cb4cc3388120c17015f7d03278aa2b848d9c1771
Lupus13A1 <symphoniga@gmail.com>
2024-06-24 09:22:10 +0700

    commit

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -52,7 +52,7 @@ const Video = () => {
         channel="custom"
         isOpen={isOpen}
         onClose={() => setOpen(false)}
-        url="/videos/my-video.mp4"
+        url="/images/video/video.mp4"
       />
 
       <div className="absolute bottom-0 left-0 right-0 z-[-1] h-full w-full bg-[url(/images/video/shape.svg)] bg-cover bg-center bg-no-repeat"></div>
